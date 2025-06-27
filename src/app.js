b03c1351881737bcf78487e1de2d4b2a7f767c63
Rahul Patil <dedx51@gmail.com>
2025-06-27 20:19:56 +0530

    Added a health check route

--- src/app.js
+++ src/app.js
@@ -14,15 +14,31 @@ validateEnv();
 connectDB();
 
 app.use(express.json());
+
+// CORS configuration
 app.use(
   cors({
-    origin: "*",
+    origin:
+      process.env.NODE_ENV === "production"
+        ? ["https://listify-iota.vercel.app/"]
+        : ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
   })
 );
+
 app.use(limiter);
+
+// health check endpoint
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "Server is running",
+    timestamp: new Date().toISOString(),
+  });
+});
 
 app.use("/api", apiRouter);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Listening on http://localhost:${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
